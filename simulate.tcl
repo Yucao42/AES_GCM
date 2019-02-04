d701c5b02fdc1ca0f4d0b3dfc0d5f08cf49780b2
Yu Cao <yu@cyclops.cs.nyu.edu>
2019-02-04 14:52:49 -0500

    change sv to verilog

--- simulate.tcl
+++ simulate.tcl
@@ -1,5 +1,5 @@
 #exec xvlog -sv aes_encrypt_stage.sv gcm_aes.sv testbench.sv
-exec xvlog -sv   clk_gen.sv test128.sv aes_signal_passing.sv aes_pipeline_stage9.sv aes_pipeline_stage8.sv aes_pipeline_stage7.sv aes_pipeline_stage6.sv aes_pipeline_stage5.sv aes_pipeline_stage4.sv aes_pipeline_stage7_pre.sv aes_pipeline_stage6_pre.sv aes_pipeline_stage5_pre.sv aes_pipeline_stage4_pre.sv aes_pipeline_stage3_pre.sv aes_pipeline_stage2_pre.sv aes_pipeline_stage3.sv aes_pipeline_stage2.sv aes_pipeline_stage12.sv aes_pipeline_stage13.sv aes_pipeline_stage14.sv aes_pipeline_stage1.sv ./fn_aes_encrypt_stage.sv ./fn_aes_key_expansion.sv fn_aes_ghash_multiplication.sv display.sv aes.sv aes_key_gen.sv gcm_aes.sv  
+exec xvlog -sv   clk_gen.sv test128.sv aes_signal_passing.sv aes_pipeline_stage9.sv aes_pipeline_stage8.sv aes_pipeline_stage7.sv aes_pipeline_stage6.sv aes_pipeline_stage5.sv aes_pipeline_stage4.sv aes_pipeline_stage7_pre.sv aes_pipeline_stage6_pre.sv aes_pipeline_stage5_pre.sv aes_pipeline_stage4_pre.sv aes_pipeline_stage3_pre.sv aes_pipeline_stage2_pre.sv aes_pipeline_stage3.sv aes_pipeline_stage2.sv  aes_pipeline_stage1.sv ./fn_aes_encrypt_stage.sv ./fn_aes_key_expansion.sv fn_aes_ghash_multiplication.sv display.sv aes.v aes_key_gen.sv gcm_aes.sv  
 #exec xelab testbench
 exec xelab -debug all testbench
 exec xsim -gui  work.testbench
