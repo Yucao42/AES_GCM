48ec7429a83285138e1d2d1fac4c1529ec033e1d
Yu Cao <yu@cyclops.cs.nyu.edu>
2019-02-04 14:46:11 -0500

    add implementation in buid_project

--- build_project.tcl
+++ build_project.tcl
@@ -12,9 +12,6 @@ read_verilog -sv  fn_aes_encrypt_stage.sv
 read_verilog -sv  fn_aes_key_expansion.sv
 read_verilog -sv  aes_signal_passing.sv
 read_verilog -sv  aes_pipeline_stage1.sv
-read_verilog -sv  aes_pipeline_stage12.sv
-read_verilog -sv  aes_pipeline_stage13.sv
-read_verilog -sv  aes_pipeline_stage14.sv
 read_verilog -sv  aes_pipeline_stage2_pre.sv
 read_verilog -sv  aes_pipeline_stage3_pre.sv
 read_verilog -sv  aes_pipeline_stage4_pre.sv
@@ -52,3 +49,21 @@ report_utilization -hierarchical  -file ./reports/synth_utilization_report_submo
 
 # show_schematic [get_nets -hier]
 write_schematic -format pdf -orientation portrait /home/yu/projects/AES_GCM/schematic_1.pdf
+
+#Run implementation
+place_design
+route_design
+
+# Reports after implementation
+report_timing -setup  -file ./reports/impl_aes_setup_report.txt
+report_timing -hold   -file ./reports/impl_aes_hold_report.txt
+report_timing_summary -file ./reports/impl_timing_report_aes.txt -delay_type min_max -max_path 50
+report_utilization    -file ./reports/impl_utilization_report.txt
+report_utilization -hierarchical  -file ./reports/impl_utilization_submod.txt
+report_power -file ./reports/impl_power_report.txt
+
+# Create bitstreams directory
+exec mkdir -p -- ./bitstreams
+
+#Write bitstream
+write_bitstream -force ./bitstreams/aes.bit
