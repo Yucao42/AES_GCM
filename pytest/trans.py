c281c74072fcde5c1ad87bdc2bf6256a42ba8514
Yucao42 <cqcy3210@gmail.com>
2019-02-11 15:43:01 -0500

    try adding 10g test

--- pytest/trans.py
+++ pytest/trans.py
@@ -1,16 +1,8 @@
-#import sys
-#import struct
-#from scapy.all import *
-
-#data = struct.pack('=BHI', 0x12, 20, 1000)
-#pkt = IP(src='192.168.200.1', dst='192.168.201.1')/UDP(sport=12345,dport=5555)/data
-#send(pkt, inter=1, count=5)
-
 """Demonstrates how to construct and send raw Ethernet packets on the
 network.
 You probably need root privs to be able to bind to the network interface,
 e.g.:
-    $ sudo python sendeth.py
+    $ sudo python trans.py
 """
 
 from socket import *
@@ -22,13 +14,13 @@ def sendeth(src, dst, eth_type, payload, interface = "eth2"):
   assert(len(src) == len(dst) == 6) # 48-bit ethernet addresses
   assert(len(eth_type) == 2) # 16-bit ethernet type
 
-  s = socket(AF_PACKET, SOCK_RAW)
+  s = socket(AF_PACKET, SOCK_RAW, htons(3))
 
   # From the docs: "For raw packet
   # sockets the address is a tuple (ifname, proto [,pkttype [,hatype]])"
 #  print('Src: {}\nDst: {}'.format(src.decode('hex'), dst.decode('hex')))
-  s.bind((interface, 1535))
-  #return s.send(dst + src + eth_type + payload)
+  s.bind((interface, 1))
+  #s.send(dst + src + eth_type + payload)
 
   #print(src + dst+ eth_type + payload)
   #print s.recv(4096)
@@ -42,7 +34,7 @@ if __name__ == "__main__":
               #"\x02\x53\x55\x4d\x45\x00",
               "\x3c\xfd\xfe\xbd\x01\xa5",
               #"\x02\x53\x55\x4d\x45\x00",
-              "\x88\x00",
+              "\x08\x01",
               #"\x7A\x05",
     "hello? I'm fine"))
     #time.sleep(1)
